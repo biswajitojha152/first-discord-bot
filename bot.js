9b6a866c11c897cb40369f170437e864fa27d3c6
Kumar Biswajit Ojha <biswajitojha152@gmail.com>
2021-09-09 21:27:12 +0530

    bot completed

--- bot.js
+++ bot.js
@@ -1,10 +1,13 @@
 require ('dotenv').config();
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS,
-Intents.FLAGS.GUILD_MESSAGES
-] });
+    Intents.FLAGS.GUILD_MESSAGES
+],
+partials: ['MESSAGE']
+});
 
-
+const BOT_PREFIX = '!';
+const MOD_ME_COMMAND = 'mod-me';
 
 
 client.on('ready',()=>{
@@ -12,13 +15,33 @@ client.on('ready',()=>{
     console.log('our bot is ready to go');
 })
 
+client.on('messageDelete',(msg)=>{
+    msg.channel.send('Stop deleting messages');
+})
+
 client.on('messageCreate',(msg)=>{
     if(msg.content==='ping'){
-        msg.reply({
-            content: 'pong!!'
-        });
+        // msg.reply({
+        //     content:'pooong!!!'
+        // });
+        msg.channel.send({content:'not tagged!!!'});
     }
+    if(msg.content === 'I Love My India'){
+        msg.react('🧡');
+    }
+    if(msg.content=== `${BOT_PREFIX}${MOD_ME_COMMAND}`){
+        // msg.member.roles.add("885540138267074560");
+        modUser(msg.member);
+
+    }
+    
+    // if(msg.content==='$mod-me'){
+    //     msg.member.roles.remove("885540138267074560");
+    // }
 })
+function modUser(member){
+    member.roles.add("885540138267074560");
+}
 
 client.login(process.env.BOT_TOKEN);
 
